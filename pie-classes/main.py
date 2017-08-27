562507d7361b2f9b7265a681fa1ca7b0d25a4125
baby-bell <baby-bell@users.noreply.github.com>
2017-08-26 21:37:06 -0700

    Add method to fetch all classes and all names

--- pie-classes/main.py
+++ pie-classes/main.py
@@ -12,10 +12,12 @@ app.config.update(dict(
     PASSWORD="default"
 ))
 
+
 def connect_db():
     db = sqlite3.connect(app.config["DATABASE"])
     db.row_factory = sqlite3.Row
     return db
+
 
 def init_db():
     db = get_db()
@@ -23,17 +25,34 @@ def init_db():
         db.cursor().executescript(f.read())
     db.commit()
 
+
 @app.cli.command("init_db")
 def init_db_command():
     init_db()
     print("Initialized the database")
+
 
 def get_db():
     if not hasattr(g, "database"):
         g.database = connect_db()
     return g.database
 
+
 @app.teardown_appcontext
 def close_db(error):
     if hasattr(g, "database"):
         g.database.close()
+
+@app.route("/", methods=["GET"])
+def get_students_and_classes():
+    import json
+    db = get_db()
+    cursor = db.execute("SELECT * FROM classes")
+    classes_and_ids = cursor.fetchall()
+    classes = {}
+    for (row_id, cls) in classes_and_ids:
+        cursor.execute("SELECT name FROM students, students_in_classes WHERE class_id=? AND student_id=id", [row_id])
+        students = sum(map(list, cursor.fetchall()), [])
+        classes[cls] = students
+
+    return json.dumps(classes)
